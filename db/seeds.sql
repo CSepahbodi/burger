009b521253fcf0c4b9cd32185ca89cfa22d2bcb4
Cyrus Sepahbodi <cyrussepahbodi@yahoo.com>
2018-09-14 12:04:40 -0700

    this commmit includes some funny seeds into the burger database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
 
-INSERT INTO burgers(burger_name) VALUES ('Brandon Burger');
+INSERT INTO burgers(burger_name) VALUES ('Brandon Burger Bruh');
 INSERT INTO burgers(burger_name) VALUES ('Buffalo Chicken Ranch Burger');
 INSERT INTO burgers(burger_name) VALUES ('Extra Vegan Veggie Burger');
 INSERT INTO burgers(burger_name) VALUES ('Double Bacon Heartbusting Cheeseburger');
 INSERT INTO burgers(burger_name) VALUES ('3 Day Old Pathetic Hamburger');
-INSERT INTO burgers(burger_name) VALUES ('MARLO BURGER');
-INSERT INTO burgers(burger_name) VALUES ('BRUCE BURGER');
+INSERT INTO burgers(burger_name) VALUES ('Marlo Burger with Extra Pickles and Cheese');
+INSERT INTO burgers(burger_name) VALUES ('Bruce Burger with Side of Bats');
