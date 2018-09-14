7399c8a37c833429141b735e0b73259be1429a01
Cyrus Sepahbodi <cyrussepahbodi@yahoo.com>
2018-09-13 21:22:20 -0700

    this commit includes a new seeeeeeeeds

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO burgers(burger_name) VALUES ('Brandon Burger');
+INSERT INTO burgers(burger_name) VALUES ('Buffalo Chicken Ranch Burger');
+INSERT INTO burgers(burger_name) VALUES ('Extra Vegan Veggie Burger');
+INSERT INTO burgers(burger_name) VALUES ('Double Bacon Heartbusting Cheeseburger');
+INSERT INTO burgers(burger_name) VALUES ('3 Day Old Pathetic Hamburger');
+INSERT INTO burgers(burger_name) VALUES ('MARLO BURGER');
+INSERT INTO burgers(burger_name) VALUES ('BRUCE BURGER');
